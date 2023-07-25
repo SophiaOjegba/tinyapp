338425a5f9b26a9b9117d7e473a4c5a95302bf78
sophia ojegba <roseysofia@yahoo.com>
2023-07-25 15:09:53 -0400

    Added the edit and delete form button and impleted the logic.

--- express_server.js
+++ express_server.js
@@ -23,6 +23,8 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -45,13 +47,94 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase.b2xVn2 };
+  const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+  
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+
+  const templateVars = { id: id, longURL: longURL };
   res.render("urls_show", templateVars);
 });
 
+// Route handler for adding new entries to the urlDatabase object
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+
+  try {
+    new URL(req.body.longURL);
+  } catch (error) {
+    return res.status(400).send(`This is not a valid url`);
+  }
+
+  const newId = generateRandomString(); 
+  const newLongURL = req.body.longURL; 
+  urlDatabase[newId] = newLongURL;
+
+
+  // Redirect the user to the show page for the newly created entry
+  res.redirect(`/urls/${newId}`);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+
+   if (!longURL) {
+     // URL with the specified 'id' not found
+     return res.status(404).send(`URL with ID ${id} not found.`);
+   }
+     delete urlDatabase[id];
+   
+
+  // Redirect the user to url page
+  res.redirect(`/urls`);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+
+   if (!longURL) {
+     // URL with the specified 'id' not found
+     return res.status(404).send(`URL with ID ${id} not found.`);
+   }
+  // 
+  const templateVars = {
+    longURL, id
+  }
+  res.render(`urls_show`, templateVars);
+});
+app.post("/urls/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+
+   if (!longURL) {
+     // URL with the specified 'id' not found
+     return res.status(404).send(`URL with ID ${id} not found.`);
+   }
+   try {
+    new URL(req.body.longURL);
+  } catch (error) {
+    return res.status(400).send(`This is not a valid url`);
+  }
+
+  const newLongURL = req.body.longURL; 
+  urlDatabase[id] = newLongURL;
+
+  res.redirect("/urls")
+});
+
+app.get("/u/:id", (req, res) => {
+   const longURL = urlDatabase[req.params.id]
+   console.log(longURL)
+   if(!longURL ){
+    res.status(404).send()
+    return;
+   }
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
