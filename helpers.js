3ca53002f23143c834f5fde0c10ee4f4e9ef6df5
sophia ojegba <roseysofia@yahoo.com>
2023-07-28 23:36:16 -0400

    Added the test directory

---
+++ helpers.js
@@ -0,0 +1,36 @@
+// Helper function email lookup
+const getUserByEmail = function(email, database) {
+  for(let user in database ){
+    if (email === database[user].email)
+    return database[user];
+  } 
+  return false;
+};
+
+//get userid corresponding to URL
+function urlsForUser(id, urlDatabase){
+  let userUrl ={}
+  for (const shortUrl in urlDatabase )
+    if (id === urlDatabase[shortUrl].userID){
+      userUrl[shortUrl] = urlDatabase[shortUrl];    
+  
+  }
+  return userUrl;
+};
+
+// Function to generate a random alphanumeric string used for short URL IDs
+function generateRandomString(){
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+  let charlength = 6
+  let randommString = "";
+
+  for (let i = 0; i < charlength; i++){
+    randonmChar = Math.floor(Math.random() * chars.length)
+    randommString += chars.substring( randonmChar, randonmChar + 1)
+  }
+  return randommString;
+};
+
+
+
+module.exports = {getUserByEmail, urlsForUser, generateRandomString};
